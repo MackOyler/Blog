337457bfc4ccdd486e9457d9eb44211e920f2d01
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2023-08-23 16:01:21 -0500

    first com. basic set-up

---
+++ src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+ return (
+    <div className="post">
+    <div className="image">
+    <img src="https://techcrunch.com/wp-content/uploads/2023/08/chandrayaan-3-landing-isro.jpg?w=990&crop=1" alt="" />
+   </div>
+   <div className="texts">
+   <h2>India’s Chandrayaan-3 makes successful landing on the moon</h2>
+   <p className="info">
+    <a className="author">Mack O. </a>
+    <time>2023-08-23 12:37pm</time>
+   </p>
+   <p className="summary">Chandrayaan-3, the latest iteration of India’s ambitious mission to the moon, has successfully landed on the lunar surface — making history after its predecessor failed in 2019.</p>
+   </div>
+   </div>
+   );
+}
